a36fdafccf9fb50d5c7431dd8cfe6c93b317d86c
Jul Jen <juliajen.prog@gmail.com>
2018-04-05 17:07:30 +0000

    automatically backed up by learn

--- db/migrate/20180405165934_create_dogs.rb
+++ db/migrate/20180405165934_create_dogs.rb
@@ -8,14 +8,3 @@ class CreateDogs < ActiveRecord::Migration
   end
 
 end
-#   def up
-#     create_table :dogs do |t|
-#       t.string :name
-#       t.string :breed
-#     end
-#   end
-#
-#   def down
-#     drop_table :dogs
-#   end
-# end
